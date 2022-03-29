4dbfce35e84f28f404f4caab6a1be2e8533572a4
V <verunchikmakarova2011@gmail.com>
2022-03-29 22:16:43 +0300

    Initial Commit

---
+++ Knapsack_local/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Knapsack_local
+//
+//  Created by Vera Makarova on 29.03.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
