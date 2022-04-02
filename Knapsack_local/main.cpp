fc224b996831a194057db12aa17dd4a4e8ab6a03
MakVM <verunchikmakarova2011@gmail.com>
2022-04-02 10:04:57 +0300

    the blue lines are annoying, but i can deal with them

--- Knapsack_local/main.cpp
+++ Knapsack_local/main.cpp
@@ -1,14 +1,17 @@
-//
-//  main.cpp
-//  Knapsack_local
-//
-//  Created by Vera Makarova on 29.03.2022.
-//
+
 
 #include <iostream>
 using namespace std;
 int main() {
 
-   cout << "Hello, Git! Well, aren't you lit?!"<<endl;
+   
+
+    
     return 0;
 }
+
+
+
+//makefile!!!!!!!
+
+
