187badc37fc2d168f172faa3c3d9c102ed546005
MakVM <verunchikmakarova2011@gmail.com>
2022-04-02 10:09:15 +0300

    n

--- Knapsack_local/main.cpp
+++ Knapsack_local/main.cpp
@@ -3,7 +3,19 @@
 #include <iostream>
 using namespace std;
 int main() {
-
+    int n; //number of items
+    int k; //maxweight
+    
+    int value, weight;
+    cin>> n;
+    cin>> k;
+    
+    for(int i = 0; i<n; i++)
+    {
+        cin>>value;
+        cin>> weight;
+        
+    }
    
 
     
